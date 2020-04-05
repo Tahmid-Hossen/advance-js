b9aecd15c84a98d9561b16d8de17762a89cf4aad
Tahmmid <aryantahmid11@gmail.com>
2020-04-05 14:02:07 +0600

    difference between null vs undefined

---
+++ null-und.js
@@ -0,0 +1,12 @@
+// Null vs Undefined
+
+// const premik = {name:"smart boy", phone:01839474220};
+// console.log(premik.gf);
+
+// let name = "Undefined";
+// console.log(name);
+
+// const a = [10, 20, 30];
+// console.log(a[4]);
+
+
