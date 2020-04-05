e70ed3c41733bd5f1dbda65d3ddbd6d1f7c709d0
Tahmmid <aryantahmid11@gmail.com>
2020-04-05 14:16:56 +0600

    difference between == vs ===

---
+++ deqvsteq.js
@@ -0,0 +1,21 @@
+// == only check the value and === check the value and value type
+
+// const a = 2;
+// const b = "2";
+
+// if(a==b){
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+
+
+const a = 2;
+const b = "2";
+
+if(a===b){
+  console.log("true");
+} else {
+  console.log("false");
+}
--- null-und.js
+++ null-und.js
@@ -9,4 +9,6 @@
 // const a = [10, 20, 30];
 // console.log(a[4]);
 
+// if u didn't keep any value into a variable then it will be null.
+
 
