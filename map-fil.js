ac074c244cda59b1078c0d8cf4199e659c8d5524
Tahmmid <aryantahmid11@gmail.com>
2020-04-05 16:35:35 +0600

    map and fielter

---
+++ map-fil.js
@@ -0,0 +1,17 @@
+const num = [10,20,30,40];
+// const output = [];
+
+// for(let i=0; i<num.length; i++) {
+//   const element = num[i];
+//   const result = element*element;
+//   output.push(result);
+// }
+// console.log(output);
+
+// mapping
+// const result = num.map(x => x*x);
+// console.log(result);
+
+// filter 
+const bigger = num.filter( x => x < 30);
+console.log(bigger);
